b6537d983c8fec8391ffea016b133e67b44b51fb
Hiroo Sugioka <hiroosugioka@hiroonombp.lan>
2019-04-03 17:16:51 +0900

    add ランキング
    １日のトークン取得数ランキングを毎日一回表示する機能を追加
    ９時に集計結果を表示した後、そのデータをmysqlへ保存する機能を追加

--- server/src/index.js
+++ server/src/index.js
@@ -3,6 +3,14 @@ const dotenv = require('dotenv');
 const LoomClient = require('./LoomClient');
 const SlackUser = require('./SlackUser');
 const CronJob = require('cron').CronJob;
+const mysql = require('mysql');
+const util = require('util');
+const connection = mysql.createConnection({
+    host : '127.0.0.1',
+    user : 'root',
+    password : '',
+    database : 'iinebot'
+});
 
 dotenv.load();
 
@@ -65,12 +73,13 @@ rtm.on('reaction_added', (event) => {
             });
         });
     },1000)
-    console.log(map.get(event.user));
+    // console.log(map.get(event.user));
 });
 
 async function aggregate() {
     let message = 'トークン獲得ランキング\n'
     let results = [];
+    let data = [];
     const users = slack.getUsers();
     for( var user of users ) {
         await loom.getBalance(user.address).then((balance) => {
@@ -82,11 +91,72 @@ async function aggregate() {
         let user_balance2 = user2["user_balance"];
         return user_balance2 - user_balance1;
     });
+    connection.connect();
     for ( i = 0; i<results.length; i++ ) {
         var result = results[i];
         message = message +  [i+1] + "位  " + result.user_balance + ' ' + result.user_name + '\n';
+        data = [];
+        data.push({name:result.user_name, points:result.user_balance});
+        connection.query("INSERT INTO daily set ?", data)
     }//左詰めで桁数が変わるたびに見えにくいので、綺麗にしたい
     slack.postMessage(message);
+    connection.end();
+    console.log(message);
+    //結果をDBから取ってきて配列に代入。
+};
+
+
+async function compare(){
+    const oldMap = new Map();
+    connection.connect();
+    connection.query = util.promisify(connection.query) 
+    try {
+      var rows = await connection.query('SELECT * FROM daily')
+      connection.end(); 
+    } catch (err) {
+      throw new Error(err)
+    }
+    for (let i = 0; i < 17; i++) {//17は参加人数が変われば変更の必要あり
+        var oldName = rows[i].name;
+        var oldPoints = rows[i].points;
+        oldMap.set(oldName, oldPoints);
+    };
+
+    const latestMap = new Map();
+    const users = slack.getUsers();
+    for( var user of users ) {
+        await loom.getBalance(user.address).then((balance) => {
+            latestMap.set(user.name, balance);
+        }); 
+    };
+    setTimeout(() => {
+    let message = '今日のトークン取得数ランキング\n'
+    let results = [];
+    for (let oKey of oldMap.keys()) {
+        for (let lKey of latestMap.keys()) {    
+            let number1 = latestMap.get(lKey);
+            let number2 = oldMap.get(oKey);
+            let dif = number1 - number2;   
+            if (oKey === lKey) {    
+                if (dif === 0) {
+                    results.push({user_name:oKey, user_balance:0});
+                    }else{
+                    results.push({user_name:oKey, user_balance:dif});
+                }
+            }
+        }
+    }
+    results.sort(function (user1, user2) {//降順にソート
+        let user_balance1 = user1["user_balance"];
+        let user_balance2 = user2["user_balance"];
+        return user_balance2 - user_balance1;
+    });
+    for ( i = 0; i<results.length; i++ ) {
+        var result = results[i];
+        message = message +  [i+1] + "位  " + result.user_balance + ' ' + result.user_name + '\n';
+    }
+    console.log(message);
+    },1000);
 };
 
     // 誰かにトークンを送る機能をつけたかったが、Solidityを変えないといけないっぽいので断念。後ほど
@@ -121,4 +191,21 @@ const job = new CronJob({
 });
 
 job.start();
+
+const rank = new CronJob({
+    /*
+    Seconds: 0-59
+    Minutes: 0-59
+    Hours: 0-23
+    Day of Month: 1-31
+    Months: 0-11
+    Day of Week: 0-6
+    */
+    cronTime: '0 55 8 * * *', // 毎日午前8時55分に送信
+    onTick: compare,
+    start: false,
+    timeZone: 'Asia/Tokyo'
+});
+
+rank.start();
 
